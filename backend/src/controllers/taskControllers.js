351ed5067dc5b2f85c73ee3c5c91d8834d5b2b6e
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-12-19 20:08:20 +0530

    #

---
+++ backend/src/controllers/taskControllers.js
@@ -0,0 +1,55 @@
+const Task = require("../models/Task");
+
+const getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const createTask = async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    const newTask = new Task({ title, description });
+    await newTask.save();
+    res.status(201).json(newTask);
+  } catch (err) {
+    res.status(400).json({
+      message: "Invalid task data",
+    });
+  }
+};
+
+const updateTask = async (req, res) => {
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    if (!updateTask) return res.status(404).json({ message: "Task not found" });
+    res.status(200).json(updatedTask);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task)
+      return res.status(404).json({
+        message: "task not found",
+      });
+    res.status(200).json({
+      message: "Task deleted Successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
+
+module.exports = { getTasks, createTask, updateTask, deleteTask };
