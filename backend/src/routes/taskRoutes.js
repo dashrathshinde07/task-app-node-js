cb2d8d8642e8b7c4742518ee485f5f0c1534a710
surwaseonkar1512 <surwaseonkar12345@gmail.com>
2024-12-20 15:43:39 +0530

    "onkar-push"

--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -1,20 +1,18 @@
 const express = require("express");
 const {
   getTasks,
-  getTasksById,
   createTask,
   updateTask,
   deleteTask,
+  getTasksById,
 } = require("../controllers/taskControllers");
-const { protect } = require("../middelwares/authMiddleware"); // Add protect middleware
 
 const router = express.Router();
 
-// Protect all task routes
-router.get("/", protect, getTasks);
-router.get("/:id", protect, getTasksById);
-router.post("/", protect, createTask);
-router.put("/:id", protect, updateTask);
-router.delete("/:id", protect, deleteTask);
+router.get("/", getTasks);
+router.get("/:id", getTasksById);
+router.post("/", createTask);
+router.put("/:id", updateTask);
+router.delete("/:id", deleteTask);
 
 module.exports = router;
