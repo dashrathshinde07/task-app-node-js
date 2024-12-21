77e64238ba3ff183658b16b3cf96ddf77fdc3c07
surwaseonkar1512 <surwaseonkar12345@gmail.com>
2024-12-21 17:15:03 +0530

    "onkar push"

--- frontend/taskv3three/src/App.js
+++ frontend/taskv3three/src/App.js
@@ -1,22 +1,28 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css';
-
+import { Provider } from "react-redux";
 import Login from "./components/ComponentsForLogin/Login";
 import AllTask from "./components/ComponentsForTaskTab/AllTask";
+import store from "./redux/store";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"; // Import CSS for toast notifications
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/tasks" element={<AllTask />} />
+    <Provider store={store}>
+      <Router>
+        <ToastContainer />
 
-          <Route path="*" element={<div>Page Not Found</div>} />
-        </Routes>
-      </div>
-    </Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/tasks" element={<AllTask />} />
+
+            <Route path="*" element={<div>Page Not Found</div>} />
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
